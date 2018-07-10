a0bab3b1164e1c0d9c77f6e741f1ab0d6afc5d8f
Abhishek Lekshmanan <abhishek@suse.com>
2018-07-10 13:55:59 +0200

    add support for creating mon nodes
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- susecloud.tf
+++ susecloud.tf
@@ -26,3 +26,26 @@ resource "openstack_compute_floatingip_associate_v2" "admin_fip" {
   floating_ip = "${openstack_networking_floatingip_v2.admin_fip.address}"
   instance_id = "${openstack_compute_instance_v2.admin.id}"
 }
+
+resource "openstack_blockstorage_volume_v2" "osds" {
+  count = "${var.osds_per_vm * var.mon_count}"
+  size = "${var.osd_size}"
+}
+
+resource "openstack_compute_instance_v2" "stmon" {
+  count = "${var.mon_count}"
+  name = "${var.vm_name_prefix}-stmon-${count.index}"
+  image_name = "${var.image_name}"
+  flavor_name = "${var.mon_flavor}"
+  security_groups = ["default"]
+  network {
+    name="fixed"
+  }
+
+}
+
+resource "openstack_compute_volume_attach_v2" "stmon-osd-attach" {
+  count = "${var.mon_count * var.osds_per_vm}"
+  instance_id = "${element(openstack_compute_instance_v2.stmon.*.id, count.index / var.osds_per_vm)}"
+  volume_id = "${element(openstack_blockstorage_volume_v2.osds.*.id, count.index)}"
+}
--- vars.tf
+++ vars.tf
@@ -6,6 +6,21 @@ variable "admin_flavor" {
   default = "m1.medium"
 }
 
+variable "mon_flavor" {
+  default = "m1.medium"
+}
+
+variable "mon_count" {
+  default = 3
+}
+
+variable "osds_per_vm" {
+  default = 4
+}
+
+variable "osd_size" {
+  default = 20
+}
 variable "deploy_key" {}
 
 variable "image_name" {
